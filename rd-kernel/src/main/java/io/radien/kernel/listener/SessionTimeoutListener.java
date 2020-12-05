872926e1c6b9333fe6ddb90d96668381f1f194ce
Marco Weiland <m.weiland@radien.io>
2020-12-05 10:29:04 +0100

    add listeners

---
+++ rd-kernel/src/main/java/io/radien/kernel/listener/SessionTimeoutListener.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2006-present openappframe.org & its legal owners. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.radien.kernel.listener;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author Marco Weiland
+ */
+public class SessionTimeoutListener implements HttpSessionListener {
+
+	protected static final String SESSION_ATTRIBUTE_USER = "user";
+	private static final Log logger = LogFactory.getLog(SessionTimeoutListener.class);
+
+	public void sessionCreated(HttpSessionEvent event) {
+		logger.debug("session created : " + event.getSession().getId());
+	}
+
+	public void sessionDestroyed(HttpSessionEvent event) {
+		HttpSession session = event.getSession();
+		try {
+			prepareLogoutInfoAndLogoutActiveUser(session);
+		} catch (Exception e) {
+			logger.error("error while logging out at session destroyed : " + e.getMessage(), e);
+		}
+	}
+
+	public void prepareLogoutInfoAndLogoutActiveUser(HttpSession httpSession) {
+		httpSession.removeAttribute(SESSION_ATTRIBUTE_USER);
+	}
+
+}
