5a73465893b8252bfae21959ae64c19d9e59c1bf
Nuno Santana <n.santana@radien.io>
2021-03-03 12:26:39 +0000

    fix test.sh

--- rd-ms-usermanagement/test.sh
+++ rd-ms-usermanagement/test.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-ACCESS_TOKEN=$(curl -L -X POST 'https://idp-int.radien.io/auth/realms/radien/protocol/openid-connect/token' \                                                                                                                                                                                                                                               17:42:27
+#to run this shell script you need jq (is a lightweight and flexible command-line JSON processor)
+# you can find jq at https://github.com/stedolan/jq
+# its available in package manager like brew (https://brew.sh/)
+if ! command -v jq &> /dev/null
+then
+    echo "to run this shell script you need jq (is a lightweight and flexible command-line JSON processor)"
+    echo "you can find jq at https://github.com/stedolan/jq"
+    echo "its available in package manager like brew (https://brew.sh/)"
+    echo "so if you have brew installed you can install jq with 'brew install jq'"
+    exit
+fi
+
+
+ACCESS_TOKEN=$(curl -L -X POST 'https://idp-int.radien.io/auth/realms/radien/protocol/openid-connect/token' \
 -H 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'client_id=radien' \
 --data-urlencode 'client_secret=d8f67579-1a33-47be-ad6f-aef38269ed12' \
