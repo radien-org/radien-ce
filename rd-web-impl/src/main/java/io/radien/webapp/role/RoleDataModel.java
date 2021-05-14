5be84d3290718acd0325f9cdce51d99f3813d764
bgamarethink <75365087+bgamarethink@users.noreply.github.com>
2021-05-14 14:18:17 +0100

    [BGAMA] Added more exceptions and messages into the role pages

--- rd-web-impl/src/main/java/io/radien/webapp/role/RoleDataModel.java
+++ rd-web-impl/src/main/java/io/radien/webapp/role/RoleDataModel.java
@@ -17,11 +17,14 @@ package io.radien.webapp.role;
 
 import io.radien.api.model.role.SystemRole;
 import io.radien.api.service.role.RoleRESTServiceAccess;
+import io.radien.ms.rolemanagement.client.entities.Role;
+import io.radien.webapp.AbstractManager;
+import io.radien.webapp.JSFUtil;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.model.LazyDataModel;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -32,23 +35,113 @@ import java.io.Serializable;
  * @author Bruno Gama
  */
 @Model
-@SessionScoped
-public class RoleDataModel implements Serializable {
+@ApplicationScoped
+public class RoleDataModel extends AbstractManager implements Serializable {
+
+    @Inject
+    private RoleRESTServiceAccess service;
 
     private LazyDataModel<? extends SystemRole> lazyModel;
 
     private SystemRole selectedRole;
 
-    @Inject
-    private RoleRESTServiceAccess service;
+    private SystemRole role = new Role();
 
     @PostConstruct
     public void init() {
-        lazyModel = new LazyRoleDataModel(service);
+        try {
+            lazyModel = new LazyRoleDataModel(service);
+        } catch (Exception e) {
+            handleError(e, JSFUtil.getMessage("rd_generic_error_message"), JSFUtil.getMessage("rd_role"));
+        }
     }
 
     public void onload() {
-        init();
+        try {
+            init();
+        } catch (Exception e) {
+            handleError(e, JSFUtil.getMessage("rd_generic_error_message"), JSFUtil.getMessage("rd_role"));
+        }
+    }
+
+    public String save(SystemRole r) {
+        try {
+            this.service.create(r);
+            handleMessage(FacesMessage.SEVERITY_INFO, JSFUtil.getMessage("rd_save_success"),
+                    JSFUtil.getMessage("rd_role"));
+            role = new Role();
+            return "roles";
+        } catch (Exception e) {
+            handleError(e, JSFUtil.getMessage("rd_save_error"), JSFUtil.getMessage("rd_role"));
+            return "role";
+        }
+    }
+
+    public String editRecords() {
+        try {
+            if (selectedRole != null) {
+                return "roleDetails";
+            } else {
+                handleMessage(FacesMessage.SEVERITY_WARN, JSFUtil.getMessage("rd_select_record_first"), JSFUtil.getMessage("rd_role"));
+            }
+            return "roles";
+        } catch (Exception e) {
+            handleError(e, JSFUtil.getMessage("rd_save_error"), JSFUtil.getMessage("rd_role"));
+            return "roleDetails";
+        }
+    }
+
+    public void delete(){
+        try {
+            if (selectedRole != null) {
+                service.delete(selectedRole.getId());
+                handleMessage(FacesMessage.SEVERITY_INFO, JSFUtil.getMessage("rd_delete_success"),
+                        JSFUtil.getMessage("rd_role"));
+            } else {
+                handleMessage(FacesMessage.SEVERITY_WARN, JSFUtil.getMessage("rd_delete_select_record_first"), JSFUtil.getMessage("rd_role"));
+            }
+        } catch (Exception e) {
+            handleError(e, JSFUtil.getMessage("rd_delete_error"), JSFUtil.getMessage("rd_role"));
+        }
+
+        role = new Role();
+        selectedRole = null;
+    }
+
+    public String returnHome() {
+        try {
+            role = new Role();
+            selectedRole = null;
+            return "roles";
+        } catch (Exception e) {
+            handleError(e, JSFUtil.getMessage("rd_delete_error"), JSFUtil.getMessage("rd_role"));
+            return "roles";
+        }
+    }
+
+    public String edit(SystemRole r) {
+        try {
+            if(r != null && !r.getName().isEmpty()) {
+                this.service.create(r);
+                role = new Role();
+                selectedRole = null;
+                return "roles";
+            } else {
+                handleMessage(FacesMessage.SEVERITY_ERROR, JSFUtil.getMessage("rd_role_name_is_mandatory"), JSFUtil.getMessage("rd_role"));
+                return "roleDetails";
+            }
+        } catch (Exception e) {
+            handleError(e, JSFUtil.getMessage("rd_edit_error"), JSFUtil.getMessage("rd_role"));
+            return "roleDetails";
+        }
+    }
+
+    public SystemRole getRole() {
+        return role;
+    }
+
+    public void setRole(SystemRole role) {
+        this.role = role;
     }
 
     public LazyDataModel<? extends SystemRole> getLazyModel() {
@@ -76,7 +169,8 @@ public class RoleDataModel implements Serializable {
     }
 
     public void onRowSelect(SelectEvent<SystemRole> event) {
-        FacesMessage msg = new FacesMessage("Customer Selected", String.valueOf(event.getObject().getId()));
+        this.selectedRole = event.getObject();
+        FacesMessage msg = new FacesMessage("Role Selected", String.valueOf(event.getObject().getId()));
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
