c0c9ef91955aafbb76eac30b8b52bf8da90f3700
Nuno Santana <n.santana@radien.io>
2021-06-24 09:42:13 +0100

    fix merge

--- rd-ms-rolemanagement/src/test/java/io/radien/ms/rolemanagement/services/LinkedAuthorizationBusinessServiceTest.java
+++ rd-ms-rolemanagement/src/test/java/io/radien/ms/rolemanagement/services/LinkedAuthorizationBusinessServiceTest.java
@@ -46,6 +46,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
