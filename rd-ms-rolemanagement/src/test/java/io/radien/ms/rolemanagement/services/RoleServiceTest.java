24f2b88f66f3d1cdeb85be61ac05e0e8f2644aee
bgamarethink <75365087+bgamarethink@users.noreply.github.com>
2021-06-29 17:53:39 +0100

    [BGAMA] Updated general importation

--- rd-ms-rolemanagement/src/test/java/io/radien/ms/rolemanagement/services/RoleServiceTest.java
+++ rd-ms-rolemanagement/src/test/java/io/radien/ms/rolemanagement/services/RoleServiceTest.java
@@ -36,7 +36,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author Bruno Gama
