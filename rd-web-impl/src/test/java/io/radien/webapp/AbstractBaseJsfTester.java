71e40c4bcc2a4e633d7e15eb33288b393b1e6249
newton.carvalho <n.carvalho@rethink.de>
2021-07-02 13:40:53 +0100

    fixing imports

--- rd-web-impl/src/test/java/io/radien/webapp/AbstractBaseJsfTester.java
+++ rd-web-impl/src/test/java/io/radien/webapp/AbstractBaseJsfTester.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright (c) 2016-present openappframe.org & its legal owners. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.radien.webapp;
 
-import io.radien.webapp.linkedAuthorization.LinkedAuthorizationManager;
-import javafx.application.Application;
-import javafx.stage.Stage;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
--- rd-web-impl/src/test/java/io/radien/webapp/linkedAuthorization/LinkedAuthorizationManagerTest.java
+++ rd-web-impl/src/test/java/io/radien/webapp/linkedAuthorization/LinkedAuthorizationManagerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016-present openappframe.org & its legal owners. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.radien.webapp.linkedAuthorization;
 
 import io.radien.api.entity.Page;
@@ -17,51 +32,28 @@ import io.radien.ms.rolemanagement.client.entities.LinkedAuthorization;
 import io.radien.ms.rolemanagement.client.entities.Role;
 import io.radien.ms.tenantmanagement.client.entities.Tenant;
 import io.radien.ms.usermanagement.client.entities.User;
-import io.radien.ms.usermanagement.client.services.UserFactory;
 import io.radien.webapp.AbstractBaseJsfTester;
 import io.radien.webapp.DataModelEnum;
-import io.radien.webapp.JSFUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.faces.application.FacesMessage;
-import javax.inject.Inject;
-import java.text.MessageFormat;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.mockito.ArgumentMatchers.anyByte;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.nullable;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyString;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.faces.context.Flash;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-
 
 /**
  * Test class referring to the manager bean {@link LinkedAuthorizationManager}
@@ -540,31 +532,4 @@ public class LinkedAuthorizationManagerTest extends AbstractBaseJsfTester {
         assertEquals(DataModelEnum.EDIT_ERROR_MESSAGE.getValue(), captured.getSummary());
         assertTrue(captured.getDetail().contains(DataModelEnum.USER_NOT_FOUND_MESSAGE.getValue()));
     }
-
-//    public String edit(SystemLinkedAuthorization l) {
-//        this.setLinkedAuthorization(l);
-//        try {
-//            this.selectedPermission = this.permissionRESTServiceAccess.getPermissionById(l.getPermissionId()).
-//                    orElseThrow(() -> new SystemException(MessageFormat.format(JSFUtil.getMessage(
-//                            "rd_permission_not_found"), l.getPermissionId())));
-//
-//            this.selectedRole = this.roleRESTServiceAccess.getRoleById(l.getRoleId()).
-//                    orElseThrow(() -> new SystemException(MessageFormat.format(JSFUtil.getMessage(
-//                            "rd_role_not_found"), l.getRoleId())));
-//
-//            this.selectedTenant = this.tenantRESTServiceAccess.getTenantById(l.getTenantId()).
-//                    orElseThrow(() -> new SystemException(MessageFormat.format(JSFUtil.getMessage(
-//                            "rd_tenant_not_found"), l.getTenantId())));
-//
-//            this.selectedUser = this.userRESTServiceAccess.getUserById(l.getRoleId()).
-//                    orElseThrow(() -> new SystemException(MessageFormat.format(JSFUtil.getMessage(
-//                            "rd_user_not_found"), l.getUserId())));
-//        }
-//        catch (Exception e) {
-//            handleError(e, JSFUtil.getMessage("rd_edit_error"), JSFUtil.getMessage("rd_linkedauthorization"));
-//        }
-//        return "linkedauthorization";
-//    }
-
-
 }
