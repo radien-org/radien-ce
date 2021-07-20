6292b58f803790ccb7cf7c888a0ba0abdfef94ca
Nuno Santana <n.santana@radien.io>
2021-07-20 20:44:51 +0100

    accesstokenutil creates tenants

--- accesstokenutil/src/main/java/io/radien/util/AccessTokenUtil.java
+++ accesstokenutil/src/main/java/io/radien/util/AccessTokenUtil.java
@@ -26,7 +26,47 @@ public class AccessTokenUtil {
                 .field("username", username)
                 .field("password", password)
                 .asObject(HashMap.class);
+        String accessToken = (String)response.getBody().get("access_token");
         System.out.println(response.getBody().get("access_token"));
 
+
+
+
+        String tenant1 ="{\"tenantKey\": \"EVCorp\", \"name\": \"Root Tenant\", \"tenantType\": \"Root\", \"tenantStart\": \"2030-01-22\", \"tenantEnd\": \"2040-01-22\"}";
+        String tenant2 ="{\"tenantKey\": \"EVCorp\", \"name\": \"Client Tenant\", \"tenantType\": \"Client\", \"tenantStart\": \"2030-01-22\", \"tenantEnd\": \"2040-01-22\", \"clientAddress\": \"Sophiestrasse 33\", \"clientZipCode\": \"38118\", \"clientCity\":\"Braunschweig\", \"clientCountry\":\"Germany\", \"clientPhoneNumber\":933876547, \"clientEmail\":\"email@email.com\", \"parentId\":1, \"clientId\":1}";
+        String tenant3 ="{\"tenantKey\": \"EVCorp\", \"name\": \"Sub Tenant\", \"tenantType\": \"Sub\", \"tenantStart\": \"2030-01-22\", \"tenantEnd\": \"2040-01-22\", \"parentId\":2, \"clientId\":2}";
+
+        String tenantUrl= "http://localhost:8082/tenantmanagementservice/v1/tenant";
+        response = Unirest.post(tenantUrl)
+                .header("Authorization", "Bearer "+accessToken)
+                .header("Content-Type","application/json")
+                .body(tenant1).asObject(HashMap.class);
+        String msg = "tentant1";
+        //checkResponse(response,msg);
+        System.out.println(msg + " " + response.getStatus());
+
+        response = Unirest.post(tenantUrl)
+                .header("Authorization", "Bearer "+accessToken)
+                .header("Content-Type","application/json")
+                .body(tenant2).asObject(HashMap.class);
+        msg = "tentant2";
+        //checkResponse(response,msg);
+        System.out.println(msg + " " + response.getStatus());
+
+        response = Unirest.post(tenantUrl)
+                .header("Authorization", "Bearer "+accessToken)
+                .header("Content-Type","application/json")
+                .body(tenant3).asObject(HashMap.class);
+        msg = "tentant3";
+        //checkResponse(response,msg);
+        System.out.println(msg + " " + response.getStatus());
+
+    }
+
+    public static void checkResponse(HttpResponse response,String msg){
+        if(!response.isSuccess()){
+            System.exit(1);
+            System.err.println(msg);
+        }
     }
 }
