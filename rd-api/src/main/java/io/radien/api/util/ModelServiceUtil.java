b9376ad181dec5d43180425ea0727ba93d2615fd
Rajesh Gavvala <rajeshgavvala@gmail.com>
2021-08-16 11:38:39 +0200

    commit - add missing java doc

--- rd-api/src/main/java/io/radien/api/util/ModelServiceUtil.java
+++ rd-api/src/main/java/io/radien/api/util/ModelServiceUtil.java
@@ -38,6 +38,15 @@ import javax.persistence.criteria.Root;
 public class ModelServiceUtil implements Serializable {
     private static final long serialVersionUID = 3114832435567898715L;
 
+    /**
+     * Filter by entity model ids to prepare query object predicate
+     * @param isFilterIds validates filter exists
+     * @param filterIds ids to searched
+     * @param filterIsLogicConjunction validates filter has logical conjunction
+     * @param criteriaBuilder query builder
+     * @param objectRoot entity model object
+     * @return An entity model based object predicate
+     */
     public static Predicate getFilteredPredicateFromModelService(boolean isFilterIds,
                                                                 List<Long>  filterIds, boolean filterIsLogicConjunction,
                                                                 CriteriaBuilder criteriaBuilder, Root<?> objectRoot){
@@ -67,28 +76,54 @@ public class ModelServiceUtil implements Serializable {
         return global;
     }
 
-    public static long getCountFromModelService(CriteriaBuilder criteriaBuilder, Predicate global, Root<?> userRoot, EntityManager em){
+    /**
+     * Existing count for a queried entity model
+     * @param criteriaBuilder query builder
+     * @param global predicate query object
+     * @param objectRoot model entity root object
+     * @param em entity manager object
+     * @return existing count of objects that belongs to a model
+     */
+    public static long getCountFromModelService(CriteriaBuilder criteriaBuilder, Predicate global, Root<?> objectRoot, EntityManager em){
         CriteriaQuery<Long> criteriaQuery = criteriaBuilder.createQuery(Long.class);
 
         criteriaQuery.where(global);
 
-        criteriaQuery.select(criteriaBuilder.count(userRoot));
+        criteriaQuery.select(criteriaBuilder.count(objectRoot));
 
         TypedQuery<Long> q=em.createQuery(criteriaQuery);
         return q.getSingleResult();
     }
 
-    public static List<Order> getListOrderSortBy(boolean isAscending, Root<?> userRoot,
+    /**
+     * Sorted list of objects based on filter
+     * @param isAscending boolean value
+     * @param objectRoot entity model root
+     * @param criteriaBuilder query builder
+     * @param sortBy list of objects to be sorted
+     * @return List of entity model objects
+     */
+    public static List<Order> getListOrderSortBy(boolean isAscending, Root<?> objectRoot,
                                                       CriteriaBuilder criteriaBuilder, List<String> sortBy){
         List<Order> orders;
         if(isAscending){
-            orders = sortBy.stream().map(i->criteriaBuilder.asc(userRoot.get(i))).collect(Collectors.toList());
+            orders = sortBy.stream().map(i->criteriaBuilder.asc(objectRoot.get(i))).collect(Collectors.toList());
         } else {
-            orders = sortBy.stream().map(i->criteriaBuilder.desc(userRoot.get(i))).collect(Collectors.toList());
+            orders = sortBy.stream().map(i->criteriaBuilder.desc(objectRoot.get(i))).collect(Collectors.toList());
         }
         return orders;
     }
 
+    /**
+     * Method that will create in the db query of where clause each and single search
+     * @param value to search/compare with query
+     * @param filterIsExact boolean value
+     * @param filterIsLogicConjunction boolean value
+     * @param criteriaBuilder query builder
+     * @param objectExpression entity model object expression
+     * @param global predicate object
+     * @return An entity object model based predicate
+     */
     public static Predicate getFieldPredicateFromModelService(Object value, boolean filterIsExact, boolean filterIsLogicConjunction,
                                              CriteriaBuilder criteriaBuilder,
                                              Expression<? extends Object> objectExpression, Predicate global){
