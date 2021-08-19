4e92f07ca2ca619e2e9dd3b941ff3af0d0cb9f82
Rajesh Gavvala <r.gavvala@rethink.de>
2021-08-19 11:16:59 +0200

    commit - remove having config properties, to avoid build issues in Remote env

--- rd-ms-usermanagement/src/test/java/io/radien/ms/usermanagement/service/KeycloakClientTest.java
+++ rd-ms-usermanagement/src/test/java/io/radien/ms/usermanagement/service/KeycloakClientTest.java
@@ -17,34 +17,106 @@ package io.radien.ms.usermanagement.service;
 
 import io.radien.ms.usermanagement.client.exceptions.RemoteResourceException;
 import io.radien.ms.usermanagement.entities.UserEntity;
-import junit.framework.TestCase;
-import org.eclipse.microprofile.config.ConfigProvider;
+
+import javax.ws.rs.core.Response;
+import kong.unirest.Unirest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KeycloakClientTest extends TestCase {
+import static org.junit.Assert.assertNotNull;
 
-    protected final static Logger log = LoggerFactory.getLogger(KeycloakClientTest.class);
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 
-    KeycloakClient client;
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore({ "javax.net.ssl.*" })
+public class KeycloakClientTest {
 
-    public void testKeycloakClient(){
+    @InjectMocks
+    private KeycloakClient keycloakClient;
+
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        keycloakClient = new KeycloakClient()
+                .clientId("CLIENT ID")
+                .username("USER NAME")
+                .password("PASSWORD")
+                .idpUrl("IDP URL")
+                .tokenPath("TOKEN/PATH")
+                .radienClientId("RADIEN CLIENT ID")
+                .radienSecret("RADIEN SECRET")
+                .radienTokenPath("RADIEN TOKEN PATH")
+                .userPath("/USER/PATH");
+
+        KeycloakClient keycloakClient1 = new KeycloakClient();
+        assertNotNull(keycloakClient1);
+
+    }
+
+
+    @Test(expected = Exception.class)
+    public void testLoginException() throws RemoteResourceException {
+        keycloakClient.login();
+    }
+
+
+    @Test(expected = Exception.class)
+    public void testCreateUserException() throws RemoteResourceException {
         UserEntity u = new UserEntity();
         u.setLogon("abc");
         u.setSub("test");
-        client = new KeycloakClient()
-                .clientId(ConfigProvider.getConfig().getValue("KEYCLOAK_CLIENT_ID", String.class))
-                .username(ConfigProvider.getConfig().getValue("KEYCLOAK_ADMIN", String.class))
-                .password(ConfigProvider.getConfig().getValue("KEYCLOAK_PASSWORD", String.class))
-                .idpUrl(ConfigProvider.getConfig().getValue("KEYCLOAK_IDP_URL", String.class))
-                .tokenPath(ConfigProvider.getConfig().getValue("KEYCLOAK_TOKEN_PATH", String.class))
-                .userPath(ConfigProvider.getConfig().getValue("KEYCLOAK_USER_PATH", String.class));
-        try {
-            client.login();
-            String id = client.createUser(KeycloakFactory.convertToUserRepresentation(u));
-            assertNotNull(id);
-        } catch (RemoteResourceException e){
-            log.error(e.getMessage(),e);
-        }
+
+        keycloakClient.createUser(KeycloakFactory.convertToUserRepresentation(u));
     }
+
+    @Test(expected = Exception.class)
+    public void testSendUpdatePasswordEmail() throws RemoteResourceException {
+        UserEntity u = new UserEntity();
+        u.setLogon("abc");
+        u.setSub("test");
+
+        keycloakClient.sendUpdatePasswordEmail(u.getSub());
+    }
+
+    @Test(expected = Exception.class)
+    public void testRefreshTokenException() throws RemoteResourceException {
+
+        keycloakClient.refreshToken();
+    }
+
+    @Test(expected = Exception.class)
+    public void testRefreshTokenWithException() throws RemoteResourceException {
+
+        keycloakClient.refreshToken(keycloakClient.getRefreshToken());
+    }
+
+    @Test(expected = Exception.class)
+    public void testUpdateUserException() throws RemoteResourceException {
+        UserEntity u = new UserEntity();
+        u.setLogon("abc");
+        u.setSub("test");
+
+        keycloakClient.updateUser("test", KeycloakFactory.convertToUserRepresentation(u));
+
+    }
+
+    @Test(expected = Exception.class)
+    public void testDeleteUserException() throws RemoteResourceException {
+        keycloakClient.deleteUser("test");
+    }
+
 }
