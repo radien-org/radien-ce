d4c128831a76f1bcf51185ee3c45ec1bb0f43976
bgamarethink <75365087+bgamarethink@users.noreply.github.com>
2021-08-27 16:07:05 +0100

    Revert "commit - remove unused rd-ms-template module"
    This reverts commit d8b909f3

---
+++ rd-ms-template/src/main/java/io/radien/ms/tenantmanagement/model/RadienModel.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package io.radien.ms.tenantmanagement.model;
+
+/**
+ * @author mawe
+ *
+ */
+public class RadienModel {
+	private String id;
+	private String message;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
