35f33ebb7455227cd25fac6a7b8afda3c41b2479
bgamarethink <75365087+bgamarethink@users.noreply.github.com>
2021-08-27 16:06:48 +0100

    Revert "commit - add implementation to remove code duplication"
    This reverts commit b7b11eb5

--- rd-ms-permissionmanagement-client/src/main/java/io/radien/ms/permissionmanagement/client/PermissionResponseExceptionMapper.java
+++ rd-ms-permissionmanagement-client/src/main/java/io/radien/ms/permissionmanagement/client/PermissionResponseExceptionMapper.java
@@ -15,17 +15,22 @@
  */
 package io.radien.ms.permissionmanagement.client;
 
-import io.radien.exception.ModelResponseExceptionMapper;
+import io.radien.exception.BadRequestException;
+import io.radien.exception.InternalServerErrorException;
+import io.radien.exception.NotFoundException;
+import io.radien.exception.TokenExpiredException;
+import org.eclipse.microprofile.rest.client.ext.ResponseExceptionMapper;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+
 /**
  * Permission mapper for the exceptions
  * @author Bruno Gama
  */
 @Provider
-public class PermissionResponseExceptionMapper extends ModelResponseExceptionMapper {
+public class PermissionResponseExceptionMapper implements ResponseExceptionMapper<Exception> {
 
     /**
      * Validates if by a given status code the error message can be handle by the following mapper
@@ -33,8 +38,12 @@ public class PermissionResponseExceptionMapper extends ModelResponseExceptionMap
      * @param headers
      * @return true in case handler can handle exception
      */
-    public boolean permissionHandles(int statusCode, MultivaluedMap<String, Object> headers) {
-        return handles(statusCode, headers);
+    @Override
+    public boolean handles(int statusCode, MultivaluedMap<String, Object> headers) {
+        return statusCode == 400        // Bad Request
+                || statusCode == 401    // Token Expiration
+                || statusCode == 404    // Not Found
+                || statusCode == 500;   // Internal Server Error
     }
 
     /**
@@ -42,7 +51,14 @@ public class PermissionResponseExceptionMapper extends ModelResponseExceptionMap
      * @param response message to be validated
      * @return a exception
      */
-    public Exception permissionToThrowable(Response response) {
-        return toThrowable(response);
+    @Override
+    public Exception toThrowable(Response response) {
+        switch(response.getStatus()) {
+            case 400: return new BadRequestException(response.readEntity(String.class));
+            case 401: return new TokenExpiredException(response.readEntity(String.class));
+            case 404: return new NotFoundException(response.readEntity(String.class));
+            case 500: return new InternalServerErrorException(response.readEntity(String.class));
+            default: return null;
+        }
     }
 }
