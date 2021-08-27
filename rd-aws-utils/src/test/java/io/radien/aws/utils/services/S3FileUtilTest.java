edb43287b6a18a3a4be87e9079e3a38a85424ccb
bgamarethink <75365087+bgamarethink@users.noreply.github.com>
2021-08-27 16:07:13 +0100

    Revert "commit - add RemoteResourceNotAvailableExceptionTest class and refactor package name for S3FileUtilTest class"
    This reverts commit 6be4abc3

--- rd-aws-utils/src/test/java/io/radien/aws/utils/services/S3FileUtilTest.java
+++ rd-aws-utils/src/test/java/io/radien/aws/utils/services/S3FileUtilTest.java
@@ -1,37 +1,17 @@
-/*
- * Copyright (c) 2016-present openappframe.org & its legal owners. All rights reserved.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package io.radien.aws.utils.s3;
+package io.radien.aws.utils.services;
 
+import io.radien.aws.utils.s3.S3FileUtil;
 import io.radien.aws.utils.s3.exceptions.RemoteResourceNotAvailableException;
-
 import junit.framework.TestCase;
-
 import org.junit.Test;
+import software.amazon.awssdk.core.exception.SdkClientException;
 
 import java.io.File;
 import java.io.IOException;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-/**
- * Class that aggregates UnitTest cases for
- * S3FileUtil
- *
- */
+
 public class S3FileUtilTest extends TestCase {
 
     //TODO: setup requirements for integration testing
