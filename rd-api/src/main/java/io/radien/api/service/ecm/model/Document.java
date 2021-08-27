764a0aa7e92caca0706e7ea77619955cd2cf357d
bgamarethink <75365087+bgamarethink@users.noreply.github.com>
2021-08-27 16:05:07 +0100

    Revert "CMS Entity creation"
    This reverts commit ee64ff0e

--- rd-api/src/main/java/io/radien/api/service/ecm/model/Document.java
+++ rd-api/src/main/java/io/radien/api/service/ecm/model/Document.java
@@ -26,6 +26,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 public class Document extends AbstractECMModel implements EnterpriseContent {
 
 	private static final long serialVersionUID = -1606222544052586973L;
+	private Long id;
 
 	/**
 	 * ECM Document constructor
@@ -62,4 +63,22 @@ public class Document extends AbstractECMModel implements EnterpriseContent {
 	public String toString() {
 		return name;
 	}
+
+	/**
+	 * ECM Document id getter
+	 * @return the document id
+	 */
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * Document id setter
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }
