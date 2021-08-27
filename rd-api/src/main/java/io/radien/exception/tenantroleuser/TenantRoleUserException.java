7acbea701eab72d0727b51903ba7dd0bad7f1618
bgamarethink <75365087+bgamarethink@users.noreply.github.com>
2021-08-27 16:06:10 +0100

    Revert "commit - refactored to correct package TenantRoleUserException.java"
    This reverts commit 0ec2ae7e

--- rd-api/src/main/java/io/radien/exception/tenantroleuser/TenantRoleUserException.java
+++ rd-api/src/main/java/io/radien/exception/tenantroleuser/TenantRoleUserException.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.radien.exception;
+package io.radien.exception.tenantroleuser;
 
 /**
  * Exception to express inconsistencies regarding TenantRoleUser domain object
