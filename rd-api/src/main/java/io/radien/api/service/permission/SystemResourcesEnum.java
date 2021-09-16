6a86a49344b958df2bd9d1ae2f6e23910e5468b3
Rajesh Gavvala <r.gavvala@rethink.de>
2021-09-16 17:11:29 +0200

    commit - add permission record for an Admin initiated email change

--- rd-api/src/main/java/io/radien/api/service/permission/SystemResourcesEnum.java
+++ rd-api/src/main/java/io/radien/api/service/permission/SystemResourcesEnum.java
@@ -31,7 +31,8 @@ public enum SystemResourcesEnum {
     TENANT_ROLE("Tenant Role"),
     TENANT_ROLE_PERMISSION("Tenant Role Permission"),
     TENANT_ROLE_USER("Tenant Role User"),
-    THIRD_PARTY_PASSWORD("Third Party Password");
+    THIRD_PARTY_PASSWORD("Third Party Password"),
+    THIRD_PARTY_EMAIL("Third Party Password");
 
     private String resourceName;
 
