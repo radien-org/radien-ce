9d11a568091d6e13f83439d569cd8cbfa7d7ba86
Nuno Santana <n.santana@radien.io>
2021-10-07 15:48:49 +0100

    remove keycloak password

--- rd-api/src/main/java/io/radien/api/KeycloakConfigs.java
+++ rd-api/src/main/java/io/radien/api/KeycloakConfigs.java
@@ -26,7 +26,6 @@ public enum KeycloakConfigs implements SystemProperties {
     ADMIN_REALM("keycloak.admin.realm"),
     ADMIN_CLIENT_ID("KEYCLOAK_CLIENT_ID"),
     ADMIN_CLIENT_SECRET("KEYCLOAK_SECRET"),
-    ADMIN_PASSWORD("KEYCLOAK_PASSWORD"),
     APP_REALM("SCRIPT_CLIENT_ID_VALUE"),
     IDP_URL("KEYCLOAK_IDP_URL"),
     TOKEN_PATH("KEYCLOAK_TOKEN_PATH"),
