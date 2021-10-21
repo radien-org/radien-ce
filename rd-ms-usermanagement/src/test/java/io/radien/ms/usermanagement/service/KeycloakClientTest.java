b06f94d1fb6e27ad6a760da8252f624a3ee4d8be
Rajesh Gavvala <r.gavvala@rethink.de>
2021-10-21 08:42:31 +0200

    commit - Test coverage for the rd-ms-usermanagement service package

--- rd-ms-usermanagement/src/test/java/io/radien/ms/usermanagement/service/KeycloakClientTest.java
+++ rd-ms-usermanagement/src/test/java/io/radien/ms/usermanagement/service/KeycloakClientTest.java
@@ -19,22 +19,26 @@ import io.radien.ms.usermanagement.client.exceptions.RemoteResourceException;
 import io.radien.ms.usermanagement.config.KeycloakEmailActions;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
+import kong.unirest.GetRequest;
+import kong.unirest.Headers;
 import kong.unirest.HttpRequestWithBody;
 import kong.unirest.HttpResponse;
-
 import kong.unirest.RequestBodyEntity;
 import kong.unirest.Unirest;
-
-import org.keycloak.representations.idm.UserRepresentation;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.keycloak.representations.idm.UserRepresentation;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -46,11 +50,12 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-
 import static org.powermock.api.mockito.PowerMockito.when;
+
 /**
  * Class that aggregates UnitTest cases for KeycloakClient
  * @author Rajesh Gavvala
@@ -62,13 +67,17 @@ public class KeycloakClientTest {
     @InjectMocks
     private KeycloakClient keycloakClient;
 
-    String inputBody_EmailVerify = "[\"" + KeycloakEmailActions.VERIFY_EMAIL + "\"]";
-    String inputBody_UpdatePassword = "[\"" + KeycloakEmailActions.UPDATE_PASSWORD + "\"]";
+    @Mock
+    private GetRequest getRequest;
 
     @Mock
     HttpRequestWithBody httpRequestWithBody;
     @Mock
     RequestBodyEntity requestBodyEntity;
+
+
+    String inputBody_EmailVerify = "[\"" + KeycloakEmailActions.VERIFY_EMAIL + "\"]";
+    String inputBody_UpdatePassword = "[\"" + KeycloakEmailActions.UPDATE_PASSWORD + "\"]";
 
     /**
      * Prepares required mock objects
@@ -81,7 +90,6 @@ public class KeycloakClientTest {
         httpRequestWithBody = PowerMockito.mock(HttpRequestWithBody.class);
         requestBodyEntity = PowerMockito.mock(RequestBodyEntity.class);
 
-
         keycloakClient = new KeycloakClient()
                 .clientId("adminClientId")
                 .clientSecret("secret")
@@ -93,12 +101,157 @@ public class KeycloakClientTest {
 
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("access_token", "TOKEN");
+        hashMap.put("refresh_token", "REFRESH TOKEN");
+        hashMap.put("grant_type", "REFRESH TOKEN");
 
         Field field = PowerMockito.field(KeycloakClient.class, "result");
         field.set(keycloakClient, hashMap);
-
     }
 
+    /**
+     * Test method {@link KeycloakClient#radienSecret(String)}
+     */
+    @Test
+    public void testRadienSecret(){
+        assertNotNull(keycloakClient.radienSecret("RADIEN SECRET"));
+    }
+
+    /**
+     * Test method {@link KeycloakClient#radienTokenPath(String)}
+     */
+    @Test
+    public void testRadienTokenPath(){
+        assertNotNull(keycloakClient.radienTokenPath("RADIEN TOKEN PATH"));
+    }
+
+    /**
+     * Test method {@link KeycloakClient#getRefreshToken()}
+     */
+    @Test
+    public void testGetRefreshToken(){
+        assertNotNull(keycloakClient.getRefreshToken());
+    }
+
+    /**
+     * Test method {@link KeycloakClient#createUser(UserRepresentation)}
+     * @throws RemoteResourceException if Unable to create User in keycloak
+     */
+    @Test
+    public void testCreateUser() throws RemoteResourceException {
+        UserRepresentation userRepresentation = mock(UserRepresentation.class);
+        HttpResponse<String> httpResponse = PowerMockito.mock(HttpResponse.class);
+        Headers headers = new Headers();
+        headers.add(HttpHeaders.LOCATION, "Location/ ");
+        when(httpResponse.getHeaders()).thenReturn(headers);
+
+        prepareMockObjectsForCreateUser(httpResponse, userRepresentation, true);
+        assertNotNull(keycloakClient.createUser(userRepresentation));
+    }
+
+
+    /**
+     * Test method {@link KeycloakClient#createUser(UserRepresentation)}
+     * @throws RemoteResourceException if Unable to create User in keycloak
+     */
+    @Test(expected = RemoteResourceException.class)
+    public void testCreateUserException() throws RemoteResourceException {
+        UserRepresentation userRepresentation = mock(UserRepresentation.class);
+        HttpResponse<String> httpResponse = PowerMockito.mock(HttpResponse.class);
+        Headers headers = new Headers();
+        when(httpResponse.getHeaders()).thenReturn(headers);
+
+        prepareMockObjectsForCreateUser(httpResponse, userRepresentation, true);
+        keycloakClient.createUser(userRepresentation);
+    }
+
+    /**
+     * Test method {@link KeycloakClient#createUser(UserRepresentation)}
+     * @throws RemoteResourceException if Unable to create User in keycloak
+     */
+    @Test(expected = RemoteResourceException.class)
+    public void testCreateUserExceptionAndHeaderIsEmpty() throws RemoteResourceException {
+        UserRepresentation userRepresentation = mock(UserRepresentation.class);
+        HttpResponse<String> httpResponse = PowerMockito.mock(HttpResponse.class);
+        Headers headers = new Headers();
+        headers.add("hello", "hello");
+        headers.add(HttpHeaders.LOCATION, "");
+        when(httpResponse.getHeaders()).thenReturn(headers);
+
+        prepareMockObjectsForCreateUser(httpResponse, userRepresentation, true);
+        keycloakClient.createUser(userRepresentation);
+    }
+
+    /**
+     * Test method {@link KeycloakClient#createUser(UserRepresentation)}
+     * @throws RemoteResourceException if Unable to create User in keycloak
+     */
+    @Test(expected = RemoteResourceException.class)
+    public void testCreateUserExceptionConflict() throws RemoteResourceException {
+        UserRepresentation userRepresentation = mock(UserRepresentation.class);
+        HttpResponse<String> httpResponse = PowerMockito.mock(HttpResponse.class);
+        when(httpResponse.getStatus()).thenReturn(409);
+        prepareMockObjectsForCreateUser(httpResponse, userRepresentation, false);
+
+        keycloakClient.createUser(userRepresentation);
+    }
+
+    /**
+     * Test method {@link KeycloakClient#createUser(UserRepresentation)}
+     * @throws RemoteResourceException if Unable to create User in keycloak
+     */
+    @Test(expected = RemoteResourceException.class)
+    public void testCreateUserExceptionConflictDoesNotMatch() throws RemoteResourceException {
+        UserRepresentation userRepresentation = mock(UserRepresentation.class);
+        HttpResponse<String> httpResponse = PowerMockito.mock(HttpResponse.class);
+        when(httpResponse.getStatus()).thenReturn(410);
+        prepareMockObjectsForCreateUser(httpResponse, userRepresentation, false);
+
+        keycloakClient.createUser(userRepresentation);
+    }
+
+
+    /**
+     * Test method {@link KeycloakClient#deleteUser(String)}
+     * @throws RemoteResourceException if resource not able to delete
+     */
+    @Test(expected = Test.None.class)
+    public void testDeleteUser() throws RemoteResourceException {
+        HttpResponse<HashMap> httpResponse = PowerMockito.mock(HttpResponse.class);
+        prepareMockObjectForDeleteUser(httpResponse, true);
+        keycloakClient.deleteUser("userSub");
+    }
+
+    /**
+     * Test method {@link KeycloakClient#deleteUser(String)}
+     * @throws RemoteResourceException if resource not able to delete
+     */
+    @Test(expected = Test.None.class)
+    public void testDeleteUser404Status() throws RemoteResourceException {
+        HttpResponse<HashMap> httpResponse = PowerMockito.mock(HttpResponse.class);
+
+        prepareMockObjectForDeleteUser(httpResponse, false);
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "User not found");
+        when(httpResponse.getStatus()).thenReturn(404);
+        when(httpResponse.getBody()).thenReturn( (HashMap) map );
+        keycloakClient.deleteUser("delete user");
+    }
+
+    /**
+     * Test method {@link KeycloakClient#deleteUser(String)}
+     * @throws RemoteResourceException if resource not able to delete
+     */
+    @Test(expected = RemoteResourceException.class)
+    public void testDeleteUserException() throws RemoteResourceException {
+        HttpResponse<HashMap> httpResponse = PowerMockito.mock(HttpResponse.class);
+
+        prepareMockObjectForDeleteUser(httpResponse, false);
+        when(httpResponse.getStatus()).thenReturn(404);
+        Map<String, String> map = new HashMap<>();
+        map.put("delete", "delete");
+        when(httpResponse.getBody()).thenReturn( (HashMap) map );
+        keycloakClient.deleteUser("delete user");
+    }
 
     /**
      * Test for method {@link KeycloakClient#sendUpdatePasswordEmail(String)}
@@ -162,6 +315,7 @@ public class KeycloakClientTest {
     @Test(expected = RemoteResourceException.class)
     public void testUpdateUserException() throws Exception {
         UserRepresentation userRepresentation = mock(UserRepresentation.class);
+
         prepareMockObjectsForUpdateUser(userRepresentation, false);
         when(Unirest.put(anyString())).thenReturn(httpRequestWithBody);
         keycloakClient.updateUser("test", userRepresentation);
@@ -195,6 +349,91 @@ public class KeycloakClientTest {
     }
 
     /**
+     * Test method {@link KeycloakClient#getSubFromEmail(String)}
+     * Asserts response fails and response body not null
+     * @throws RemoteResourceException if any exception to a remote method call
+     */
+    @Test(expected = RemoteResourceException.class)
+    public void testGetSubFromEmailResponseSuccessFailGetBodyNull() throws RemoteResourceException {
+        HttpResponse<ArrayList> httpResponse = PowerMockito.mock(HttpResponse.class);
+        prepareMockObjectForGetSubFromEmail(httpResponse, false);
+        List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("userEmail", "email@email.com");
+        mapList.add(map);
+        when(httpResponse.getBody()).thenReturn( (ArrayList) mapList );
+
+        keycloakClient.getSubFromEmail("email@email.com");
+    }
+
+    /**
+     * Test method {@link KeycloakClient#getSubFromEmail(String)}
+     * Asserts response as success
+     * @throws RemoteResourceException if any exception to a remote method call
+     */
+    @Test(expected = Test.None.class)
+    public void testGetSubFromEmailResponseSuccess() throws RemoteResourceException {
+        HttpResponse<ArrayList> httpResponse = PowerMockito.mock(HttpResponse.class);
+        prepareMockObjectForGetSubFromEmail(httpResponse, true);
+        List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("userEmail", "email@email.com");
+        mapList.add(map);
+        when(httpResponse.getBody()).thenReturn( (ArrayList) mapList );
+
+        keycloakClient.getSubFromEmail("email@email.com");
+    }
+
+    /**
+     * Test method {@link KeycloakClient#getSubFromEmail(String)}
+     * Asserts response and its response body results more than one
+     * @throws RemoteResourceException if any exception to a remote method call
+     */
+    @Test(expected = Test.None.class)
+    public void testGetSubFromEmailResponseSuccessResultsSizeMoreThanOne() throws RemoteResourceException {
+        HttpResponse<ArrayList> httpResponse = PowerMockito.mock(HttpResponse.class);
+        prepareMockObjectForGetSubFromEmail(httpResponse, true);
+        List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("userEmail", "email@email.com");
+        Map<String, Object> map1 = new HashMap<>();
+        map.put("userEmail", "email@email.com");
+        mapList.add(map);
+        mapList.add(map1);
+        when(httpResponse.getBody()).thenReturn( (ArrayList) mapList );
+
+        keycloakClient.getSubFromEmail("email");
+    }
+
+    /**
+     * Test method {@link KeycloakClient#getSubFromEmail(String)}
+     * @throws RemoteResourceException if any exception to a remote method call
+     */
+    @Test(expected = Test.None.class)
+    public void testGetSubFromEmailResponseSuccessResultsEmpty() throws RemoteResourceException {
+        HttpResponse<ArrayList> httpResponse = PowerMockito.mock(HttpResponse.class);
+        prepareMockObjectForGetSubFromEmail(httpResponse, true);
+        List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
+        when(httpResponse.getBody()).thenReturn( (ArrayList) mapList );
+
+        keycloakClient.getSubFromEmail("email");
+    }
+
+    /**
+     * Test method {@link KeycloakClient#getSubFromEmail(String)}
+     * @throws RemoteResourceException if any exception to a remote method call
+     */
+    @Test(expected = RemoteResourceException.class)
+    public void testGetSubFromEmailResponseFail() throws RemoteResourceException {
+        HttpResponse<ArrayList> httpResponse = PowerMockito.mock(HttpResponse.class);
+        prepareMockObjectForGetSubFromEmail(httpResponse, true);
+        prepareMockObjectForGetSubFromEmail(httpResponse, false);
+
+        keycloakClient.getSubFromEmail( "email@email.com" );
+    }
+
+
+    /**
      * Prepares mock objects for the invoke methods
      * @param response to be set either true/false
      * @param inputBody to be set
@@ -213,6 +452,25 @@ public class KeycloakClientTest {
         when(httpRequestWithBody.body(inputBody)).thenReturn(requestBodyEntity);
         when(requestBodyEntity.asObject(String.class)).thenReturn(httpResponse);
         when(Unirest.put(anyString())).thenReturn(httpRequestWithBody);
+    }
+
+    /**
+     * Prepares mock objects for the invoke methods
+     * @param userRepresentation object information
+     */
+    private void prepareMockObjectsForCreateUser(HttpResponse<String> httpResponse, UserRepresentation userRepresentation, boolean response) {
+        if(response){
+            when(httpResponse.isSuccess()).thenReturn(Boolean.TRUE);
+        } else {
+            when(httpResponse.isSuccess()).thenReturn(Boolean.FALSE);
+        }
+
+        when(httpRequestWithBody.header(HttpHeaders.AUTHORIZATION, "Bearer TOKEN")).thenReturn(httpRequestWithBody);
+        when(httpRequestWithBody.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)).thenReturn(httpRequestWithBody);
+        when(httpRequestWithBody.body(userRepresentation)).thenReturn(requestBodyEntity);
+        when(Unirest.post(anyString())).thenReturn( httpRequestWithBody );
+        when(requestBodyEntity.asObject(String.class)).thenReturn(httpResponse);
+
     }
 
     /**
@@ -258,5 +516,40 @@ public class KeycloakClientTest {
         when(requestBodyEntity.asString()).thenReturn(httpResponse);
 
         when(Unirest.put(anyString())).thenReturn(httpRequestWithBody);
+    }
+
+    /**
+     * Prepares mock objects for the invoke methods for getSubFromEmail()
+     * @param httpResponse mock object
+     * @param response boolean flag
+     */
+    private void prepareMockObjectForGetSubFromEmail(HttpResponse<ArrayList> httpResponse, boolean response) {
+        if(response){
+            when(httpResponse.isSuccess()).thenReturn(Boolean.TRUE);
+        } else {
+            when(httpResponse.isSuccess()).thenReturn(Boolean.FALSE);
+        }
+        when(getRequest.header(HttpHeaders.AUTHORIZATION, "Bearer TOKEN")).thenReturn(getRequest);
+        when(getRequest.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)).thenReturn(getRequest);
+        when(Unirest.get(anyString())).thenReturn(getRequest);
+        when(getRequest.asObject(ArrayList.class)).thenReturn(httpResponse);
+    }
+
+    /**
+     * Prepares mock objects for the invoke methods for deleteUser()
+     * @param httpResponse mock object
+     * @param response boolean flag
+     */
+    private void prepareMockObjectForDeleteUser(HttpResponse<HashMap> httpResponse, boolean response) {
+
+        if(response){
+            when(httpResponse.isSuccess()).thenReturn(Boolean.TRUE);
+        } else {
+            when(httpResponse.isSuccess()).thenReturn(Boolean.FALSE);
+        }
+        when(httpRequestWithBody.header(HttpHeaders.AUTHORIZATION, "Bearer TOKEN")).thenReturn(httpRequestWithBody);
+        when(httpRequestWithBody.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)).thenReturn(httpRequestWithBody);
+        when(Unirest.delete(anyString())).thenReturn( httpRequestWithBody );
+        when(httpRequestWithBody.asObject(HashMap.class)).thenReturn(httpResponse);
     }
 }
