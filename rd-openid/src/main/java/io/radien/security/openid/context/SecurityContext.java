e8304ed3158161af9330aab6f00612495d4a3286
newton.carvalho <n.carvalho@rethink.de>
2021-11-13 13:29:17 +0000

    removing unused imports

--- rd-openid/src/main/java/io/radien/security/openid/context/SecurityContext.java
+++ rd-openid/src/main/java/io/radien/security/openid/context/SecurityContext.java
@@ -15,9 +15,6 @@
  */
 package io.radien.security.openid.context;
 
-import com.nimbusds.oauth2.sdk.id.State;
-import com.nimbusds.oauth2.sdk.token.AccessToken;
-import com.nimbusds.oauth2.sdk.token.RefreshToken;
 import io.radien.security.openid.model.UserDetails;
 import java.io.Serializable;
 
