0613752bb1a253a8428f3f09ce2717e9c71f9c6f
Nuno Santana <n.santana@radien.io>
2022-02-02 11:43:55 +0000

    fix unit test in pipeline

--- rd-ms-usermanagement/src/test/java/io/radien/ms/usermanagement/service/KeycloakClientFactoryTest.java
+++ rd-ms-usermanagement/src/test/java/io/radien/ms/usermanagement/service/KeycloakClientFactoryTest.java
@@ -1,10 +1,14 @@
 package io.radien.ms.usermanagement.service;
 
+import io.radien.api.KeycloakConfigs;
+import io.radien.api.OAFProperties;
 import io.radien.ms.usermanagement.client.exceptions.RemoteResourceException;
 import kong.unirest.HttpRequestWithBody;
 import kong.unirest.HttpResponse;
 import kong.unirest.MultipartBody;
 import kong.unirest.Unirest;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,10 +22,12 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 
+import static io.radien.api.OAFProperties.RADIEN_ENV;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.reflect.Whitebox.setInternalState;
 
 @RunWith(PowerMockRunner.class)
 @PowerMockIgnore({ "javax.net.ssl.*" })
@@ -37,11 +43,33 @@ public class KeycloakClientFactoryTest {
         MultipartBody multipartBody = mock(MultipartBody.class);
         HttpRequestWithBody requestWithBody = mock(HttpRequestWithBody.class);
         HttpResponse<HashMap> response = mock(HttpResponse.class);
-        when(Unirest.post(argThat("http://localhost:8080/TOKEN"::contentEquals))).thenReturn(requestWithBody);
+        Config config =mock(Config.class);
+
+        String idpUrl = "http://localhost:8080";
+        String rdTokenPath = "/TOKEN";
+        String endpointUrl = idpUrl + rdTokenPath;
+        String rdClientId = "CLIENT";
+        String rdClientSecret = "SECRET";
+        String rdUserPath = "/USER";
+
+
+        when(config.getValue(KeycloakConfigs.TOKEN_PATH.propKey(),String.class)).thenReturn(rdTokenPath);
+        when(config.getValue(KeycloakConfigs.USER_PATH.propKey(),String.class)).thenReturn(rdUserPath);
+        when(config.getValue(KeycloakConfigs.ADMIN_CLIENT_ID.propKey(),String.class)).thenReturn(rdClientId);
+        when(config.getValue(KeycloakConfigs.ADMIN_CLIENT_SECRET.propKey(),String.class)).thenReturn(rdClientSecret);
+        when(config.getValue(KeycloakConfigs.RADIEN_CLIENT_ID.propKey(),String.class)).thenReturn(rdClientId);
+        when(config.getValue(KeycloakConfigs.RADIEN_SECRET.propKey(),String.class)).thenReturn(rdClientSecret);
+        when(config.getValue(KeycloakConfigs.RADIEN_TOKEN_PATH.propKey(),String.class)).thenReturn(rdTokenPath);
+        when(config.getValue(OAFProperties.RADIEN_ENV.propKey(),String.class)).thenReturn("LOCAL");
+        when(config.getValue(KeycloakConfigs.IDP_URL.propKey(),String.class)).thenReturn(idpUrl);
+        setInternalState(this.clientFactory, "config", config);
+
+
+        when(Unirest.post(argThat(endpointUrl::contentEquals))).thenReturn(requestWithBody);
         when(requestWithBody.header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED)).
                 thenReturn(requestWithBody);
-        when(requestWithBody.field("client_id", "CLIENT")).thenReturn(multipartBody);
-        when(multipartBody.field("client_secret", "SECRET")).thenReturn(multipartBody);
+        when(requestWithBody.field("client_id", rdClientId)).thenReturn(multipartBody);
+        when(multipartBody.field("client_secret", rdClientSecret)).thenReturn(multipartBody);
         when(multipartBody.field("grant_type", "client_credentials")).thenReturn(multipartBody);
         when(multipartBody.asObject(HashMap.class)).thenReturn(response);
 
