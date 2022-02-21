9eb86873eae1189c860ee25b748810e7cab520c6
Nuno Santana <n.santana@radien.io>
2022-02-21 10:33:49 +0000

    improve compatibility with windows

---
+++ rebuildKeycloakImage.bat
@@ -0,0 +1,4 @@
+docker stop rkc
+docker rm rkc
+docker rmi radien-keycloak
+docker build -t radien-keycloak -f keycloak\local\Dockerfile .
---
+++ rebuildTomeeImage.bat
@@ -0,0 +1,5 @@
+mvn clean install -DskipTests
+docker stop radien-tomee1
+docker rm radien-tomee1
+docker rmi radien-tomee
+docker build -t radien-tomee -f tomee\local\Dockerfile .
