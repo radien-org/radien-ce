b8eeed4aff9aced1087d8b3743aeb0777c18d10e
Rajesh Gavvala <r.gavvala@rethink.de>
2022-02-22 17:36:09 +0100

    commit-fix test class SPIThemesResourceProviderTest.java

--- rd-ms-keycloak-themes/src/test/java/io/radien/spi/themes/SPIThemesResourceProviderTest.java
+++ rd-ms-keycloak-themes/src/test/java/io/radien/spi/themes/SPIThemesResourceProviderTest.java
@@ -15,12 +15,15 @@
  */
 package io.radien.spi.themes;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Locale;
+import javax.ws.rs.InternalServerErrorException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,11 +38,16 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ URL.class })
+@PrepareForTest({ SPIThemesResourceProvider.class, URL.class })
 public class SPIThemesResourceProviderTest {
     private static final Logger log = LoggerFactory.getLogger(SPIThemesResourceProviderTest.class);
 
@@ -51,29 +59,44 @@ public class SPIThemesResourceProviderTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        spiThemesResourceProvider = new SPIThemesResourceProvider(keycloakSession);
+        new SPIThemesResourceProvider(keycloakSession);
     }
 
     @Test
     public void testGetMessage() throws Exception {
-        class UrlWrapper {
+        String content = "{\n" +
+                "  \"title\": \"Title\"\n" +
+                "}";
+        InputStream inputStream = new ByteArrayInputStream(content.getBytes());
+        HttpURLConnection urlConnection = PowerMockito.mock(HttpURLConnection.class);
+        URL finalUrl = PowerMockito.mock(URL.class);
 
-            final URL url;
 
-            public UrlWrapper(String spec) throws MalformedURLException {
-                url = new URL(spec);
-            }
+        PowerMockito.whenNew(URL.class).withArguments(anyString()).thenReturn(finalUrl);
+        PowerMockito.when(finalUrl.openConnection()).thenReturn(urlConnection);
+        PowerMockito.when(urlConnection.getInputStream()).thenReturn(inputStream);
+        PowerMockito.when(urlConnection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
 
-            public URLConnection openConnection() throws IOException {
-                return url.openConnection();
-            }
-        }
+        assertEquals(1,spiThemesResourceProvider.getMessages("",new Locale("en")).size());
+    }
 
-        UrlWrapper url = Mockito.mock(UrlWrapper.class);
-        HttpURLConnection huc = Mockito.mock(HttpURLConnection.class);
-        PowerMockito.when(url.openConnection()).thenReturn(huc);
-        assertTrue(url.openConnection() instanceof HttpURLConnection);
-        spiThemesResourceProvider.getMessages("",new Locale("en"));
+    @Test
+    public void testGetMessageNoContent() throws Exception {
+        String content = "{\n" +
+                "  \"title\": \"Title\"\n" +
+                "}";
+        InputStream inputStream = new ByteArrayInputStream(content.getBytes());
+        HttpURLConnection urlConnection = PowerMockito.mock(HttpURLConnection.class);
+        URL finalUrl = PowerMockito.mock(URL.class);
+
+
+        PowerMockito.whenNew(URL.class).withArguments(anyString()).thenReturn(finalUrl);
+        PowerMockito.when(finalUrl.openConnection()).thenReturn(urlConnection);
+        PowerMockito.when(urlConnection.getInputStream()).thenReturn(inputStream);
+        PowerMockito.when(urlConnection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_NO_CONTENT);
+
+        assertEquals(0,spiThemesResourceProvider.getMessages("",new Locale("en")).size());
+
     }
 
     @Test
