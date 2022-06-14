7e67315f84b7b35c9c5bbee507f1db7d54dccf02
ajain119 <106659116+ajain119@users.noreply.github.com>
2022-06-14 18:31:35 +0200

    Visual changes in accident type component

--- front-end-workspace/protec-angular-workspace/projects/protec/src/app/app.module.ts
+++ front-end-workspace/protec-angular-workspace/projects/protec/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
