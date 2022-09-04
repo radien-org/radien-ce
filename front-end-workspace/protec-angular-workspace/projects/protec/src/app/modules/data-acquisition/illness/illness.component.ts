820e24f81821a3142bae7ec94580ca55ca4df9a3
k.sabino-at-384544062592 <7ectTYX2KWrL1DFj0twGyJpdvmAcOui/gXlyKJtlhXY=>
2022-09-05 00:58:52 +0100

    fix illness forms

--- front-end-workspace/protec-angular-workspace/projects/protec/src/app/modules/data-acquisition/illness/illness.component.ts
+++ front-end-workspace/protec-angular-workspace/projects/protec/src/app/modules/data-acquisition/illness/illness.component.ts
@@ -10,25 +10,45 @@ import { LOCAL } from '../../../shared/services/storage/local.enum';
 })
 export class IllnessComponent implements OnInit {
 
+  initNavigation = [{
+    label: this.translationService.instant('ZURÜCK'),
+    link: '/data-acquisition/details-intro'
+  }]
+
   pageNav = {
     navegation: {
       type: 'navegation-buttons',
-      navegations: [
-        {
-          label: this.translationService.instant('ZURÜCK'),
-          link: '/data-acquisition/details-intro'
-        }
-      ]
+      navegations: [...this.initNavigation]
     }
   }
 
   accidentType: string = '';
+
+  accidentUccur: any;
+  professionalActivity: any;
+  employer: any;
+  professionalActivity12Months = false;
+  previousJobOccupation: any;
+  previousEmployer: any;
+  moreThan12Months = false;
 
   constructor(private readonly translationService: TranslateService, private readonly storageService: StorageService) { }
 
   ngOnInit(): void {
     this.accidentType = this.storageService.getItem(LOCAL.ACCIDENT_TYPE);
     this.verifyAccidentType();
+    const savedData = this.storageService.getItem(LOCAL.ILLNESS_DETAIL_FORM)?.data;
+    console.log('debug (savedData):', savedData);
+    if(savedData) {
+      this.accidentUccur = savedData.accidentUccur;
+      this.professionalActivity = savedData.professionalActivity;
+      this.employer = savedData.employer;
+      this.professionalActivity12Months = savedData.professionalActivity12Months;
+      this.previousJobOccupation = savedData.previousJobOccupation;
+      this.previousEmployer = savedData.previousEmployer;
+      this.moreThan12Months = savedData.moreThan12Months;
+    }
+    this.verifyInput();
   }
 
   verifyAccidentType() {
@@ -36,32 +56,59 @@ export class IllnessComponent implements OnInit {
     if(this.accidentType === 'recreational-accident') {
       this.pageNav.navegation.navegations.push(
         {
-          label: this.translationService.instant('weiter'),
-          link: '/data-acquisition/full-body'
+          label: this.translationService.instant('WEITER'),
+          link: '/data-acquisition/illness-diagnostic'
         }
       );
-    } else if(this.accidentType === 'disease') {
+    } else if(this.accidentType === 'recreational-accident') {
       this.pageNav.navegation.navegations.push(
         {
-          label: this.translationService.instant('weiter'),
-          link: '/data-acquisition/summary'
-        }
-      );
-    } else {
-      this.pageNav.navegation.navegations.push(
-        {
-          label: this.translationService.instant('weiter'),
+          label: this.translationService.instant('WEITER'),
           link: '/data-acquisition/illness-diagnostic'
         }
       );
     }
   }
 
-  option_class = 'hide';
-  public showOptions(){
-    this.option_class = 'show-flex'
+  verifyInput(): void {
+    console.log('saving...');
+    if (this.accidentUccur && this.professionalActivity) {
+      this.pageNav.navegation.navegations = [...this.initNavigation, {
+        label: this.translationService.instant('WEITER'),
+        link: '/data-acquisition/illness-diagnostic'
+      }]
+      this.save();
+    } else {
+      this.pageNav.navegation.navegations = [...this.initNavigation]
+    }
   }
+
+  public showOptions(){
+    this.professionalActivity12Months = true;
+    this.save();
+  }
+
   public hideOptions(){
-    this.option_class = 'hide'
+    this.professionalActivity12Months = false;
+    this.save();
+  }
+
+  choiceRadioOption(value:boolean) {
+    this.moreThan12Months = value;
+    this.save();
+  }
+
+  save() {
+    const data = {data: {
+      accidentUccur: this.accidentUccur,
+      professionalActivity: this.professionalActivity,
+      employer: this.employer,
+      professionalActivity12Months: this.professionalActivity12Months,
+      previousJobOccupation: this.previousJobOccupation,
+      previousEmployer: this.previousEmployer,
+      moreThan12Months: this.moreThan12Months
+    }};
+    console.log('debug (save):', data);
+    this.storageService.setItem(LOCAL.ILLNESS_DETAIL_FORM, data);
   }
 }
