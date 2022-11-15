0d3346e0c3ac0a5daeac650c65bdb31cd8701b97
a.sousa <a.sousa@rethink.de>
2022-11-15 12:27:21 +0000

    Added logging to client util.

--- rd-ms-notificationmanagement-client/src/main/java/io/radien/ms/notificationmanagement/client/util/ClientServiceUtil.java
+++ rd-ms-notificationmanagement-client/src/main/java/io/radien/ms/notificationmanagement/client/util/ClientServiceUtil.java
@@ -5,10 +5,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import javax.enterprise.context.RequestScoped;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RequestScoped
 public class ClientServiceUtil {
+    private static final Logger log = LoggerFactory.getLogger(ClientServiceUtil.class);
+
     public EmailNotificationResourceClient getEmailNotificationResourceClient(String urlStr) throws MalformedURLException {
+        log.info(urlStr);
         URL url = new URL(urlStr);
         return RestClientBuilder
                 .newBuilder()
