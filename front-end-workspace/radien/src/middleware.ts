12ead4cdfec45acc2d6c8576d86559b90a1fee4b
a.sousa <a.sousa@rethink.de>
2023-02-12 18:54:57 +0000

    Added middleware logic to protect pages. TODO: How to generalize protected paths; How to add error message to client.

--- front-end-workspace/radien/src/middleware.ts
+++ front-end-workspace/radien/src/middleware.ts
@@ -1,24 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { withLocale } from "@/middleware/withLocale";
+import { stackMiddlewares } from "@/middleware/stackMiddlewares";
+import { withAuth } from "@/middleware/withAuth";
 
-export async function middleware(req: NextRequest) {
-    if (req.nextUrl.pathname.startsWith("/_next") || req.nextUrl.pathname.includes("/api/")) {
-        return;
-    }
+const middlewares = [withAuth, withLocale];
 
-    if (req.nextUrl.locale === "default") {
-        const locale = req.cookies.get("NEXT_LOCALE")?.value || "en";
-
-        const response = NextResponse.redirect(new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url));
-        response.cookies.set("NEXT_LOCALE", locale);
-        return response;
-    } else {
-        const locale = req.nextUrl.locale;
-        const localeCookie = req.cookies.get("NEXT_LOCALE")?.value || "en";
-
-        const response = NextResponse.redirect(new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url));
-        if (locale !== localeCookie) {
-            response.cookies.set("NEXT_LOCALE", locale);
-            return response;
-        }
-    }
-}
+export default stackMiddlewares(middlewares);
