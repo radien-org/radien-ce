528c99f4357d03dcceeffd9b9fb657c892809855
a.sousa <a.sousa@rethink.de>
2023-02-17 22:00:27 +0000

    Added dark more support for cards

--- front-end-workspace/radien/src/components/Card/Card.tsx
+++ front-end-workspace/radien/src/components/Card/Card.tsx
@@ -14,7 +14,9 @@ export default function Card(props: CardProps) {
     return (
         <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <Link href={href} locale={locale}>
-                <article className="overflow-hidden rounded-lg shadow-lg bg-[#faf7f7] text-[#00142b] hover:bg-[#00142b] hover:text-[#faf7f7]">
+                <article
+                    className="overflow-hidden rounded-lg shadow-lg bg-container-light text-light hover:bg-[#00142b] hover:text-[#faf7f7]
+                    dark:bg-container-dark dark:text-text-dark hover:dark:bg-container-light hover:dark:text-text-light">
                     <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                         <h1 className="text-xxl font-semibold">{title}</h1>
                     </header>
--- front-end-workspace/radien/tailwind.config.js
+++ front-end-workspace/radien/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                container: {
+                    light: "#faf7f7",
+                    dark: "var(--color-grey-800-8gx340)",
+                },
+                text: {
+                    light: "#00142b",
+                    dark: "var(--color-grey-300-gjxrd6)",
+                },
+            },
+        },
     },
     plugins: [],
 };
